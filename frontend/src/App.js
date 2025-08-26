783abbea3e637c152719e8c2799c35905c9121ba
spoorthi <mspoor0515@gmail.com>
2025-08-26 19:48:39 +1000

    changes are made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,24 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
+import React from 'react';
+import ProjectsPage from './components/ProjectsPage';
 
 function App() {
   return (
+    <div className="App">
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<ProjectsPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
-export default App;
+export default App;
